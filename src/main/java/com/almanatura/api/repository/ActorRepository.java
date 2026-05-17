8ec2b52388ce0472da9cf47410f6d5081c001038
Alexis Campusano <ale.cmpb@gmail.com>
2026-05-16 20:44:04 -0400

    fix: apply spotless formatting to ActorRepository query

--- src/main/java/com/almanatura/api/repository/ActorRepository.java
+++ src/main/java/com/almanatura/api/repository/ActorRepository.java
@@ -12,9 +12,11 @@ public interface ActorRepository extends JpaRepository<Actor, Long> {
 
     boolean existsByFullNameIgnoreCase(String fullName);
 
-    @Query("SELECT DISTINCT a FROM Actor a " +
-           "JOIN ProjectApplication pa ON pa.actor.id = a.id " +
-           "JOIN Project p ON pa.project.id = p.id " +
-           "WHERE p.pillar = :pillar")
-    List<Actor> findByProjectPillar(@Param("pillar") com.almanatura.api.enums.ProjectPillar pillar);
+    @Query(
+            "SELECT DISTINCT a FROM Actor a "
+                    + "JOIN ProjectApplication pa ON pa.actor.id = a.id "
+                    + "JOIN Project p ON pa.project.id = p.id "
+                    + "WHERE p.pillar = :pillar")
+    List<Actor> findByProjectPillar(
+            @Param("pillar") com.almanatura.api.enums.ProjectPillar pillar);
 }
